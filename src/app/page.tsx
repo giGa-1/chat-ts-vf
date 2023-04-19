2ee8a2d9141c915a5ffbc5ae430234bcfcf69365
Shmeb <1gledpobgornuk@gmail.com>
2023-04-19 20:29:02 +0300

    implementing database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import { Inter } from 'next/font/google'
 import Button from '@/components/UI/Button'
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+export default  function Home() {
   return (
     <div>
-      <Button>hello</Button>
+      <Button >hello</Button>
     </div>
   )
 }
